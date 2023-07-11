5d1d868d7958f6fb6412692027833655371971ee
Xoohyun <ehowl3609@gmail.com>
2023-07-11 15:50:55 +0900

    React Merging

--- src/main/reactJs/my-app/src/router/RouteMain.js
+++ src/main/reactJs/my-app/src/router/RouteMain.js
@@ -1,38 +1,4 @@
-<<<<<<< HEAD
-import React from 'react';
-import {Route, Routes, useLocation} from 'react-router-dom';
-import {Main} from '../pages/main';
-import {DevChat, Lobby, Room} from '../pages/test';
-import {Footer, Header, NotFound} from '../components';
-import {SignIn} from "../pages/signin";
 
-function RouteMain(props) {
-    const location = useLocation();
-    const noHeaderFooterRoutes = ['/signin',
-        path => path.startsWith('/devchat')
-    ];
-
-    const showHeaderFooter = !noHeaderFooterRoutes.some(route=>
-    typeof route === 'function'? route(location.pathname) : route === location.pathname);
-
-    return (
-        <>
-            {showHeaderFooter && <Header/>}
-            <Routes>
-                <Route path='/' element={<Main/>}/>
-                <Route path='/home' element={<Main/>}/>
-                <Route path={'/signin'} element={<SignIn/>}/>
-
-                <Route path='/devchat' element={<DevChat/>}>
-                    <Route path={':ai_idx'} element={<DevChat/>}/>
-                </Route>
-
-                <Route path='*' element={<NotFound/>}/>
-            </Routes>
-            {showHeaderFooter && <Footer/>}
-        </>
-    );
-=======
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Main } from "../pages/main";
@@ -74,7 +40,7 @@ function RouteMain(props) {
       </Route>
     </Routes>
   );
->>>>>>> 650d23de1d2d797c299ab3a7572dbb4fb347635a
+
 }
 
 export default RouteMain;
