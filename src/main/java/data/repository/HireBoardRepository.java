7d7a30f80c321f7bbc5d098960b3151b36c38764
Hyun Oh <you@example.com>
2023-07-14 15:19:31 +0900

    conflict : merge conflict 수정

--- src/main/java/data/repository/HireBoardRepository.java
+++ src/main/java/data/repository/HireBoardRepository.java
@@ -2,6 +2,8 @@ package data.repository;
 
 import data.entity.HireBoardEntity;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HireBoardRepository extends JpaRepository<HireBoardEntity, Integer> {
     Long countBy();
+
     // Page<HireBoardEntity> findAll(Pageable pageable);
     // public HireBoardEntity findByHbIdx(int hbIdx);
 
--- src/main/java/jwt/setting/config/SecurityConfig.java
+++ src/main/java/jwt/setting/config/SecurityConfig.java
@@ -70,11 +70,8 @@ public class SecurityConfig {
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
                 .antMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
-<<<<<<< HEAD
                 .antMatchers("/member/sign-up/**","/oauth2/callback/**","/hboard/**").permitAll() // 회원가입 접근 가능
-=======
                 .antMatchers("/member/sign-up/**","/oauth2/callback/**","/compmember/sign-up/**").permitAll() // 회원가입 접근 가능
->>>>>>> 7b7a75428dd1380475ef5cb9bb65651acabac07e
                 .anyRequest().authenticated() // 위의 경로 이외에는 모두 인증된 사용자만 접근 가능
                 .and()
 
