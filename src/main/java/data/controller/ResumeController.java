936d06c053384177aa793575b18c3d974ddef7b4
kddongkyu <113885745+kddongkyu@users.noreply.github.com>
2023-07-18 12:46:16 +0900

    feat : resume 번역 api 추가

--- src/main/java/data/controller/ResumeController.java
+++ src/main/java/data/controller/ResumeController.java
@@ -1,15 +1,15 @@
 package data.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import data.dto.resume.ResumeWrapper;
 import data.service.ResumeService;
-import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.javassist.bytecode.Mnemonic;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 @RestController
@@ -40,7 +40,12 @@ public class ResumeController {
 
     @GetMapping("/{m_idx}")
     public ResponseEntity<ResumeWrapper> getOneResume(@PathVariable int m_idx) {
-        return new ResponseEntity<ResumeWrapper>(resumeService.getOneResume(m_idx),HttpStatus.OK);
+        ResumeWrapper resumeWrapper = resumeService.getOneResume(m_idx);
+        if(resumeWrapper.getResumeDto() == null) {
+            return new ResponseEntity<ResumeWrapper>(resumeService.getOneResume(m_idx),HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<ResumeWrapper>(resumeService.getOneResume(m_idx),HttpStatus.OK);
+        }
     }
 
     @DeleteMapping("/{m_idx}")
@@ -52,4 +57,11 @@ public class ResumeController {
     public ResponseEntity<String> updateResume(@RequestBody ResumeWrapper resumeWrapper, HttpSession session) {
         return new ResponseEntity<String>(resumeService.updateResume(resumeWrapper.getResumeDto(),resumeWrapper.getResumeCareerDtoList(), resumeWrapper.getResumeLicenseDtoList(),session), HttpStatus.OK);
     }
+
+    @PostMapping("/translate")
+    public ResponseEntity<String> translate(@RequestBody JsonNode jsonNode) throws IOException {
+        return new ResponseEntity<String>(resumeService.translateResume(jsonNode.get("text").asText()),HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/data/service/ResumeService.java
+++ src/main/java/data/service/ResumeService.java
@@ -12,15 +12,21 @@ import data.repository.resume.ResumeLicenseRepository;
 import data.repository.resume.ResumeRepository;
 import lombok.extern.slf4j.Slf4j;
 import naver.cloud.NcpObjectStorageService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
-import java.util.Collections;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,8 +38,15 @@ public class ResumeService {
     private final ResumeLicenseRepository resumeLicenseRepository;
     private final NcpObjectStorageService storageService;
 
+
     @Value("${aws.s3.bucketName}")
     private String bucketName;
+
+    @Value("${naver.translate.client_id}")
+    private String client_id;
+
+    @Value("${naver.translate.client_secret}")
+    private String client_secret;
 
     public ResumeService(ResumeRepository resumeRepository, NcpObjectStorageService storageService, ResumeCareerRepository resumeCareerRepository, ResumeLicenseRepository resumeLicenseRepository) {
         this.resumeRepository = resumeRepository;
@@ -69,7 +82,7 @@ public class ResumeService {
             resumeLicenseRepository.save(resumeLicenseEntity);
         }
         log.info("이력서 자격증 등록완료.");
-        
+
         session.removeAttribute("file");
         session.removeAttribute("refile");
         log.info("이력서 최종 등록완료.");
@@ -84,7 +97,7 @@ public class ResumeService {
         }
         session.setAttribute("file", file);
         log.info("이력서 파일 업로드 완료");
-        
+
         return "이력서 파일 업로드 완료";
     }
 
@@ -98,7 +111,7 @@ public class ResumeService {
 
         return "자격증, 포트폴리오 파일 업로드 완료";
     }
-    
+
     public ResumeWrapper getOneResume(int m_idx) {
         ResumeWrapper resumeWrapper = new ResumeWrapper();
 
@@ -135,7 +148,6 @@ public class ResumeService {
 
         resumeWrapper.setResumeLicenseDtoList(resumeLicenseDtoList);
         log.info("resumeLicenseDto 값 가져오기 및 삽입 완료");
-
 
         return resumeWrapper;
     }
@@ -210,6 +222,47 @@ public class ResumeService {
         log.info("이력서 최종 업데이트 완료.");
 
         return "이력서 업데이트 완료.";
+    }
+
+    public String translateResume(String inputText) throws IOException {
+        try {
+            String text = URLEncoder.encode(inputText, "UTF-8");
+            String apiURL = "https://naveropenapi.apigw.ntruss.com/nmt/v1/translation";
+            URL url = new URL(apiURL);
+            HttpURLConnection con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("X-NCP-APIGW-API-KEY-ID", client_id);
+            con.setRequestProperty("X-NCP-APIGW-API-KEY", client_secret);
+            // post request
+            String postParams = "source=ko&target=en&text=" + text;
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(postParams);
+            wr.flush();
+            wr.close();
+            int responseCode = con.getResponseCode();
+            BufferedReader br;
+            if(responseCode==200) { // 정상 호출
+                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            } else {  // 오류 발생
+                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+            }
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = br.readLine()) != null) {
+                response.append(inputLine);
+            }
+            br.close();
+
+            String responseJson = response.toString();
+
+            JSONObject obj = new JSONObject(responseJson);
+            String translatedText = obj.getJSONObject("message").getJSONObject("result").getString("translatedText");
+
+            return translatedText;
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
 }
